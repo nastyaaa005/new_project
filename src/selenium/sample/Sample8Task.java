26fea5d59c7ba49c4872fae02070cf335ce76b0c
student <student@student.com>
2018-08-03 11:54:20 +0300

    Sample8Task

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -36,6 +36,14 @@ public class Sample8Task {
     public void styleChecks() throws Exception {
 //         TODO:
 //        check the background of top 2 sections
+        WebElement sectionRed = driver.findElement(By.className("w3-pale-red"));
+        assertEquals("rgba(255, 221, 221, 1)", sectionRed.getCssValue("background-color"));
+
+        WebElement sectionYellow = driver.findElement(By.className("w3-pale-yellow"));
+        assertEquals("rgba(255, 255, 204, 1)", sectionYellow.getCssValue("background-color"));
+
 //        check h1 element font-size 64px
+        WebElement h1 = driver.findElement(By.xpath("//h1"));
+        assertEquals("64px", h1.getCssValue("font-size"));
     }
 }
