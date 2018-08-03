9709f456ca2752c0064f875a558ca9e59717d0bf
student <student@student.com>
2018-08-03 17:21:25 +0300

    OlegAtask

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,7 +3,10 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertTrue;
@@ -31,12 +34,40 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+
+        WebElement enterText = driver.findElement(By.id("numb"));
+        WebElement SubmitButton = driver.findElement(By.className("w3-btn"));
+        WebElement resultText = driver.findElement(By.id("ch1_error"));
+        enterText.clear();
+        String text = "abc";
+        enterText.sendKeys(text);
+        SubmitButton.click();
+        assertEquals("Please enter a number", resultText.getText());
+//        Alert correctalert = driver.switchTo().alert();
+//        assertEquals("Please enter a number", correctalert.getText());
+
+
+
+
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+
+        WebElement enterNum = driver.findElement(By.id("numb"));
+        WebElement SubmitButton = driver.findElement(By.className("w3-btn"));
+        WebElement resultText = driver.findElement(By.id("ch1_error"));
+        enterNum.clear();
+        int num = 25;
+        enterNum.sendKeys(String.valueOf(num));
+        SubmitButton.click();
+        assertEquals("Number is too small", resultText.getText());
+
+
+
+
     }
 
     @Test
@@ -45,6 +76,17 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement enterNum = driver.findElement(By.id("numb"));
+        WebElement SubmitButton = driver.findElement(By.className("w3-btn"));
+        WebElement resultText = driver.findElement(By.id("ch1_error"));
+        enterNum.clear();
+        int num = 700;
+        enterNum.sendKeys(String.valueOf(num));
+        SubmitButton.click();
+        assertEquals("Number is too big", resultText.getText());
+
+
+
     }
 
     @Test
@@ -52,10 +94,38 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        WebElement enterNum = driver.findElement(By.id("numb"));
+        WebElement SubmitButton = driver.findElement(By.className("w3-btn"));
+        WebElement resultText = driver.findElement(By.id("ch1_error"));
+        enterNum.clear();
+        int num = 81;
+        enterNum.sendKeys(String.valueOf(num));
+        SubmitButton.click();
+        Alert myAlert = driver.switchTo().alert();
+        myAlert.getText();
+        assertEquals("Square root of 81 is 9.00",myAlert.getText());
+
+
+
+
     }
 
     @Test
     public void correctSquareRootWithRemainder() {
+
+        WebElement enterNum = driver.findElement(By.id("numb"));
+        WebElement SubmitButton = driver.findElement(By.className("w3-btn"));
+        WebElement resultText = driver.findElement(By.id("ch1_error"));
+        enterNum.clear();
+        int num = 98;
+        enterNum.sendKeys(String.valueOf(num));
+        SubmitButton.click();
+        Alert myAlert = driver.switchTo().alert();
+        myAlert.getText();
+        assertEquals("Square root of 98 is 9.90",myAlert.getText());
+
+
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
