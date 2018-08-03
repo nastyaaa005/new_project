108618e9e0c539833df6dc52984d652264dd1fca
student <student@student.com>
2018-08-03 15:36:50 +0300

    Task3

--- src/selenium/tasks/Task3.java
+++ src/selenium/tasks/Task3.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,9 +28,21 @@ public class Task3 {
     @Test
     public void feedback() {
 // TODO:
+        String name = "John";
+        String age = "22";
 //        fill in "name", "age", select 1 or more language, select genre, select option, add a comment
+        driver.findElement(By.id("fb_name")).sendKeys(name);
+        driver.findElement(By.id("fb_age")).sendKeys(age);
+        driver.findElement(By.cssSelector(".w3-check[type='checkbox'][value='English']")).click();
+        driver.findElement(By.cssSelector(".w3-radio[type='radio'][value='male']")).click();
+        Select dropdown = new Select(driver.findElement(By.name("option")));
+        dropdown.selectByVisibleText("Why me?");
+        driver.findElement(By.name("comment")).sendKeys("A comment");
 //        check that the button send is blue with white letters
+        WebElement sendButton = driver.findElement(By.cssSelector(".w3-btn-block[type='submit']"));
+        assertEquals("rgba(33, 150, 243, 1)", sendButton.getCssValue("background-color"));
 //        click "send"
+        driver.findElement(By.cssSelector(".w3-btn-block[type='submit']")).click();
 //        check that the feedback was filled correctly
 //        check that the button yes is green and no is red but both have white letters
     }
