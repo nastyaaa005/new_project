dc9dfe75a4df99468762f233c1310c4e2727c028
Viktorija-R <41744741+Viktorija-R@users.noreply.github.com>
2018-08-06 23:02:41 +0300

    Task 3 done

--- src/selenium/tasks/Task3.java
+++ src/selenium/tasks/Task3.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -31,12 +34,14 @@ public class Task3 {
     @Test
     public void feedback() {
         WebElement nameField = driver.findElement(By.id("fb_name"));
-        String sendKeyOne = "abc";
-        nameField.sendKeys(sendKeyOne);
+        String nameText = "abc";
+        nameField.sendKeys(nameText);
+        assertEquals(nameText, nameField.getAttribute("value"));
 
         WebElement ageField = driver.findElement(By.id("fb_age"));
-        String sendKeyTwo = "12";
-        ageField.sendKeys(sendKeyTwo);
+        String ageText = "12";
+        ageField.sendKeys(ageText);
+        assertEquals(ageText, ageField.getAttribute("value"));
 
         List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
 
@@ -48,108 +53,97 @@ public class Task3 {
         English.click();
         WebElement French = driver.findElement(By.cssSelector(".w3-check[value='French'][type=checkbox]"));
         WebElement Spanish = driver.findElement(By.cssSelector(".w3-check[value='Spanish'][type=checkbox]"));
+        Spanish.click();
         WebElement Chinese = driver.findElement(By.cssSelector(".w3-check[value='Chinese'][type=checkbox]"));
         assertFalse(French.isSelected());
-        assertFalse(Spanish.isSelected());
+        assertTrue(Spanish.isSelected());
         assertFalse(Chinese.isSelected());
         assertTrue(English.isSelected());
 
-        List<WebElement> Radios = driver.findElements(By.cssSelector(".w3-radio[type='radio']"));
 
-        for (WebElement oneRadio : Radios) {
-            assertFalse(oneRadio.isSelected());
-        }
+        WebElement Male = driver.findElement(By.cssSelector(".w3-radio[value='male'][type='radio']"));
+        WebElement Female = driver.findElement(By.cssSelector(".w3-radio[value='female'][type='radio']"));
+        WebElement DontKnow = driver.findElement(By.cssSelector(".w3-radio:nth-of-type(3)"));
 
-        WebElement Male = driver.findElement(By.cssSelector(".w3-radio[value='male'][type='checkbox']"));
-        WebElement Female = driver.findElement(By.cssSelector(".w3-radio[value='female'][type='checkbox']"));
-        WebElement DontKnow = driver.findElement(By.xpath("//a[.='Don't know (Disabled)']"));
+        assertFalse(Male.isSelected());
+        assertTrue(DontKnow.isSelected());
+        assertFalse(Female.isSelected());
+        assertFalse(DontKnow.isEnabled());
+
         Female.click();
 
         assertFalse(Male.isSelected());
         assertFalse(DontKnow.isSelected());
         assertTrue(Female.isSelected());
-    }
-    @Test
-    public void selectOptionByValue() {
+
 
         Select dropdown = new Select(driver.findElement(By.id("like_us")));
+
         assertEquals("Choose your option", dropdown.getFirstSelectedOption().getText());
         dropdown.selectByValue("Why me?");
         assertEquals("Why me?", dropdown.getFirstSelectedOption().getText());
-    }
-        @Test
-        public void addComment() {
 
-            WebElement textInput = driver.findElement(By.className("comment"));
+            WebElement textInput = driver.findElement(By.name("comment"));
 
-            String sendKeyOne = "qwerty";
-            textInput.sendKeys(sendKeyOne);
-        }
+            String commentText = "qwerty";
+            textInput.sendKeys(commentText);
+            assertEquals(commentText, textInput.getAttribute("value"));
 
-        @Test
-                public void buttonSend() {
-            assertEquals("rgb(33, 150, 243)",
+
+            assertEquals("rgba(33, 150, 243, 1)",
                     driver.findElement(By.xpath("//*[@class='w3-btn-block w3-blue w3-section']"))
                             .getCssValue("background-color"));
 
-            assertEquals("rgb(255, 255, 255)",
+            assertEquals("rgba(255, 255, 255, 1)",
                     driver.findElement(By.xpath("//*[@class='w3-btn-block w3-blue w3-section']"))
                             .getCssValue("color"));
 
-            WebElement sendButton = driver.findElement(By.className("w3-btn-block w3-blue w3-section"));
+            WebElement sendButton = driver.findElement(By.xpath("//*[@class='w3-btn-block w3-blue w3-section']"));
             sendButton.click();
-        }
 
-        @Test
-                public void fbCheck() {
             WebElement name = driver.findElement(By.id("name"));
-            String textInput = "abc";
-            assertEquals(textInput,name.getText());
+            assertEquals(nameText, name.getText());
 
             WebElement age = driver.findElement(By.id("age"));
-            String ageInput = "12";
-            assertEquals(ageInput,age.getText());
+            assertEquals(ageText,age.getText());
 
             WebElement language = driver.findElement(By.id("language"));
-            String languageInput = "English";
-            assertEquals(languageInput,language.getText());
+            assertEquals("English,Spanish",language.getText());
 
             WebElement gender = driver.findElement(By.id("gender"));
-            String genderInput = "female";
-            assertEquals(genderInput,gender.getText());
+            assertEquals("female",gender.getText());
 
             WebElement option = driver.findElement(By.id("option"));
-            String optionInput = "Why me?";
-            assertEquals(optionInput,option.getText());
+            assertEquals("Why me?",option.getText());
 
             WebElement comment = driver.findElement(By.id("comment"));
-            String commentInput = "qwerty";
-            assertEquals(commentInput,comment.getText());
+            assertEquals("qwerty",comment.getText());
 
-        }
-        assertEquals("rgb(76, 175, 80)",
+
+        assertEquals("rgba(76, 175, 80, 1)",
                 driver.findElement(By.xpath("//*[@class='w3-btn w3-green w3-xlarge']"))
                         .getCssValue("background-color"));
 
-        assertEquals("rgb(255, 255, 255)",
+        assertEquals("rgba(255, 255, 255, 1)",
                 driver.findElement(By.xpath("//*[@class='w3-btn w3-green w3-xlarge']"))
                         .getCssValue("color"));
 
-        assertEquals("rgb(244, 67, 54)",
+        assertEquals("rgba(244, 67, 54, 1)",
                 driver.findElement(By.xpath("//*[@class='w3-btn w3-red w3-xlarge']"))
                         .getCssValue("background-color"));
 
-        assertEquals("rgb(255, 255, 255)",
+        assertEquals("rgba(255, 255, 255, 1)",
                 driver.findElement(By.xpath("//*[@class='w3-btn w3-red w3-xlarge']"))
                         .getCssValue("color"));
-
-
-
-// TODO:
-//        fill in "name", "age", select 1 or more language, select genre, select option, add a comment
-//        check that the button send is blue with white letters
-//        click "send"
-//        check that the feedback was filled correctly
-//        check that the button yes is green and no is red but both have white letters
+//
+//
+//
+//// TODO:
+////        fill in "name", "age", select 1 or more language, select genre, select option, add a comment
+////        check that the button send is blue with white letters
+////        click "send"
+////        check that the feedback was filled correctly
+////        check that the button yes is green and no is red but both have white letters
+//    }
     }
 }
