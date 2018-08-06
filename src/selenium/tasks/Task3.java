66464f1d0c4fd0bfe9d6e52d42120e7368ec72f9
student <student@student.com>
2018-08-06 13:39:54 +0300

    Task3

--- src/selenium/tasks/Task3.java
+++ src/selenium/tasks/Task3.java
@@ -30,6 +30,7 @@ public class Task3 {
 // TODO:
         String name = "John";
         String age = "22";
+
 //        fill in "name", "age", select 1 or more language, select genre, select option, add a comment
         driver.findElement(By.id("fb_name")).sendKeys(name);
         driver.findElement(By.id("fb_age")).sendKeys(age);
@@ -38,11 +39,15 @@ public class Task3 {
         Select dropdown = new Select(driver.findElement(By.name("option")));
         dropdown.selectByVisibleText("Why me?");
         driver.findElement(By.name("comment")).sendKeys("A comment");
+
 //        check that the button send is blue with white letters
         WebElement sendButton = driver.findElement(By.cssSelector(".w3-btn-block[type='submit']"));
         assertEquals("rgba(33, 150, 243, 1)", sendButton.getCssValue("background-color"));
+        assertEquals(("rgba(255, 255, 255, 1)"), sendButton.getCssValue("color"));
+
 //        click "send"
-        driver.findElement(By.cssSelector(".w3-btn-block[type='submit']")).click();
+        sendButton.click();
+
 //        check that the feedback was filled correctly
         assertEquals(name, driver.findElement(By.id("name")).getText());
         assertEquals(age, driver.findElement(By.id("age")).getText());
@@ -50,12 +55,18 @@ public class Task3 {
         assertEquals("male", driver.findElement(By.id("gender")).getText());
         assertEquals("Why me?", driver.findElement(By.id("option")).getText());
         assertEquals("A comment", driver.findElement(By.id("comment")).getText());
+
 //        check that the button yes is green and no is red but both have white letters
-        WebElement yesButton = driver.findElement(By.cssSelector(".w3-green"));
-        WebElement noButton = driver.findElement(By.cssSelector(".w3-red"));
+        WebElement yesButton = driver.findElement(By.cssSelector(".w3-green[onclick='openFeedback()']"));
+        WebElement noButton = driver.findElement(By.cssSelector(".w3-red[onclick='window.history.back();']"));
         assertEquals(("rgba(76, 175, 80, 1)"), yesButton.getCssValue("background-color"));
         assertEquals(("rgba(255, 255, 255, 1)"), yesButton.getCssValue("color"));
         assertEquals(("rgba(244, 67, 54, 1)"), noButton.getCssValue("background-color"));
         assertEquals(("rgba(255, 255, 255, 1)"), yesButton.getCssValue("color"));
+
+//        check that the "thank you" message is green with white letters
+        yesButton.click();
+        String thankYouMessage = "Thank you, " + name + ", for your feedback!";
+        assertEquals(thankYouMessage,driver.findElement(By.id("message")).getText());
     }
 }
